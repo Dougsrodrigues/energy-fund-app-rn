d3accb3422da80796f910e549e65334d248901a0
Douglas <dougsrodrigues@outlook.com>
2023-07-09 17:54:54 +0200

    feat: init create sign in

--- src/modules/app/routes/auth-routes.tsx
+++ src/modules/app/routes/auth-routes.tsx
@@ -1,15 +1,24 @@
 import { SignIn } from "@/modules/authentication/pages";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useTheme } from "styled-components/native";
 
 type AuthtackParamList = {
   SignIn: undefined;
 };
 
 const AuthStack = createStackNavigator<AuthtackParamList>();
-
 export const AuthStackNavigator = () => {
+  const theme = useTheme();
+
   return (
-    <AuthStack.Navigator>
+    <AuthStack.Navigator
+      screenOptions={{
+        headerTitle: "",
+        cardStyle: {
+          backgroundColor: theme.colors.white,
+        },
+      }}
+    >
       <AuthStack.Screen name="SignIn" component={SignIn} />
     </AuthStack.Navigator>
   );
--- src/modules/authentication/pages/sign-in.tsx
+++ src/modules/authentication/pages/sign-in.tsx
@@ -1,5 +1,32 @@
-import { Text } from "react-native";
+import { Typography } from "@/modules/app/components";
+import { horizontalScale, verticalScale } from "@/modules/app/utils/functions";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { styled } from "styled-components/native";
 
 export const SignIn = () => {
-  return <Text>SIIIIGNNNN IIINNNNN</Text>;
+  return (
+    <Container>
+      <SafeAreaViewStyled>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <Content>
+            <Typography variant="title">Login</Typography>
+          </Content>
+        </TouchableWithoutFeedback>
+      </SafeAreaViewStyled>
+    </Container>
+  );
 };
+
+const SafeAreaViewStyled = styled.KeyboardAvoidingView`
+  flex: 1;
+`;
+const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+`;
+
+const Content = styled.View`
+  flex: 1;
+  align-items: center;
+
+  padding: ${verticalScale(16)}px ${horizontalScale(16)}px;
+`;
