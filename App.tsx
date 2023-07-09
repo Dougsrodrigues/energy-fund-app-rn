3a8abb822132b1b35996ce9e1a5347c1bfdb0c2c
Douglas <dougsrodrigues@outlook.com>
2023-07-09 15:31:00 +0200

    chore: add styled components and expo fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { horizontalScale } from "@/modules/app/utils/functions";
+import {
+  useFonts,
+  Sora_500Medium,
+  Sora_600SemiBold,
+  Sora_400Regular,
+} from "@expo-google-fonts/sora";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Sora_500Medium,
+    Sora_600SemiBold,
+    Sora_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
